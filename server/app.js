7baf57eb444a2d11b27318bf4efc52cdfcc5cd58
Christopher Brady <chrisbot95@gmail.com>
2024-02-21 20:36:15 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80; // You can change this to any port you prefer
+
+// Serve static files (including index.html) from the 'public' directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
