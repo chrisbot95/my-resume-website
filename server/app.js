19c1d10e9ecf97428d026eef4b37a0c2638f14cf
Christopher Brady <chrisbot95@gmail.com>
2024-02-21 20:45:46 -0500

    changed port number

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 80; // You can change this to any port you prefer
+const port = 443; // You can change this to any port you prefer
 
 // Serve static files (including index.html) from the 'public' directory
 app.use(express.static(path.join(__dirname, '..', 'public')));
